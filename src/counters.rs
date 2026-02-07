dd3c998f7ffc22cba8331a429f601efd46615c92
scottfowler <scott@fwlr.io>
2026-02-08 01:42:40 +1100

    finished tutorial

---
+++ src/counters.rs
@@ -0,0 +1,45 @@
+use leptos::prelude::*;
+use leptos_mview::mview;
+use tracing::info;
+
+#[component]
+fn ProgressBar(
+    #[prop(default = 100)] max: u16,
+    progress: impl Fn() -> i32 + Send + Sync + 'static,
+) -> impl IntoView {
+    mview! {
+        progress {max} value={progress};
+    }
+}
+
+#[component]
+pub fn Counters() -> impl IntoView {
+    let (count, set_count) = signal(0);
+    let increment = move |_| {
+        info!("incremented");
+        *set_count.write() += 1;
+    };
+    let double_count = move || count() * 2;
+    let progress = move || count() * 3;
+
+    mview! {
+        button on:click={increment} class:bg-src-red-700=[count() % 2 == 1] (
+            "Click me: " {count}
+        )
+        button
+            on:click={move |_| set_count(count() + 1)}
+                class:"bg-src-green-700"=[count() % 2 == 0]
+        (
+            "Or click me: " {count}
+        )
+        button on:click={increment} class:bg-src-blue-700=[count() % 2 == 1] (
+            "Or even click me: " {count}
+        )
+        p ("Double count:" {double_count} )
+        ProgressBar progress={count};
+        ProgressBar max=50 progress={double_count};
+        ProgressBar max=25 {progress};
+        a href="foo" ("foo")
+        a href="bar" ("bar")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,24 @@
+use fwlr_io::app::App;
 use leptos::mount::mount_to_body;
-use tracing::info;
-use tracing_subscriber::fmt::format;
-use tracing_subscriber_wasm::MakeConsoleWriter;
-
-mod app;
-use app::App;
 
 fn main() {
     tracing_subscriber::fmt()
         .event_format(
-            format()
+            tracing_subscriber::fmt::format()
                 .with_ansi(false)
-                .with_target(false)
                 .with_level(false)
                 .compact(),
         )
-        .with_writer(MakeConsoleWriter::default().map_trace_level_to(tracing::Level::DEBUG))
+        .with_writer(
+            tracing_subscriber_wasm::MakeConsoleWriter::default()
+                .map_trace_level_to(tracing::Level::DEBUG),
+        )
         .without_time()
         .init();
+
     console_error_panic_hook::set_once();
 
-    info!("mounting");
+    tracing::info!("mounting");
 
     mount_to_body(App)
 }

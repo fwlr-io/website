aa6fa2d039fd66164f870af9224e77532b0e7cdb
scottfowler <scott@fwlr.io>
2026-02-08 08:18:09 +1100

    document todos

--- src/content.rs
+++ src/content.rs
@@ -1,20 +1,14 @@
 use leptos::prelude::*;
 use leptos_mview::mview;
 
+// todo: combine Exposition and Details
+// todo: just delete Summary, Exposition, Details
+
 #[component]
 pub fn Summary(children: Children) -> impl IntoView {
     mview! {
         div #summary class="h-full w-1/2 flex-none overflow-y-scroll overscroll-y-contain" (
             {children()}
-        )
-    }
-}
-
-#[component]
-pub fn Anchor(text: &'static str) -> impl IntoView {
-    mview! {
-        a href=f["#{text}"] (
-            {text}
         )
     }
 }
@@ -29,18 +23,33 @@ pub fn Exposition(children: Children) -> impl IntoView {
 }
 
 #[component]
-pub fn Section(anchor: &'static str, children: Children) -> impl IntoView {
+pub fn Details(children: Children) -> impl IntoView {
     mview! {
-        section id={anchor} class="border-src-350 target:bg-src-800 m-4 rounded-md border p-4" (
+        div #details class="h-full w-1/2 flex-none overflow-y-scroll overscroll-y-contain py-[50vh]" (
             {children()}
         )
     }
 }
 
+// todo:
+//  some way to pass in one string, e.g. "pharetra"
+//  and get back Pharetra::Anchor, Pharetra::Section / Pharetra::Aside
+// HOCs? slots? a struct?
+// Anchor<'pharetra'> ?
+
 #[component]
-pub fn Details(children: Children) -> impl IntoView {
+pub fn Anchor(text: &'static str) -> impl IntoView {
     mview! {
-        div #details class="h-full w-1/2 flex-none overflow-y-scroll overscroll-y-contain py-[50vh]" (
+        a href=f["#{text}"] (
+            {text}
+        )
+    }
+}
+
+#[component]
+pub fn Section(anchor: &'static str, children: Children) -> impl IntoView {
+    mview! {
+        section id={anchor} class="border-src-350 target:bg-src-800 m-4 rounded-md border p-4" (
             {children()}
         )
     }

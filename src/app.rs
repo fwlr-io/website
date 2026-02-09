3d7646c6abe3510987ef381329e22127781dbf09
scottfowler <scott@fwlr.io>
2026-02-09 15:40:39 +1100

    start ux

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,10 @@
-use crate::content::Content;
 use leptos::prelude::*;
 use leptos_mview::mview;
+// use crate::counters::Counters;
+// use crate::content::Content;
+use crate::ux::*;
 
 #[component]
 pub fn App() -> impl IntoView {
-    mview! { Content; }
+    mview! {}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
+// #![feature(impl_trait_in_bindings)]
+// #![feature(impl_trait_in_assoc_type)]
+// #![feature(impl_trait_in_fn_trait_return)]
+// #![feature(inherent_associated_types)]
+// pub mod failed_hoc_attempt;
+
 pub mod app;
 pub mod content;
 pub mod counters;
-// pub mod failed_hoc_attempt;
+pub mod ux;

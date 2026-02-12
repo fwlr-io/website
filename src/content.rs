9a2604253e1913d385ed27f1ab9b06f8690f07ad
scottfowler <scott@fwlr.io>
2026-02-12 13:26:33 +1100

    codeblocks

--- src/content.rs
+++ src/content.rs
@@ -4,7 +4,7 @@ use leptos_mview::mview;
 #[component]
 pub fn Content() -> impl IntoView {
     view! {
-        <body class="flex overflow-x-scroll flex-row font-serif h-dvh w-dvw snap-x snap-mandatory bg-base-a text-base-e">
+        <body class="flex overflow-x-scroll flex-row font-serif text-black bg-white h-dvh w-dvw snap-x snap-mandatory">
             <div
                 id="summary"
                 class="overflow-y-scroll overscroll-y-contain w-1/2 h-full py-[50vh] shrink-0 snap-center ml-[50%]"
@@ -21,7 +21,7 @@ pub fn Content() -> impl IntoView {
             >
                 <section
                     id="pharetra"
-                    class="p-1 my-3 target:bg-base-b rounded-1"
+                    class="p-1 my-3 target:bg-dim-white rounded-1"
                 >
                     <p>
                         "Pharetra ex gravida congue. Pellentesque ornare lacus quis quam "
@@ -33,7 +33,7 @@ pub fn Content() -> impl IntoView {
                 </section>
                 <section
                     id="nullam"
-                    class="p-1 my-3 target:bg-base-b rounded-1"
+                    class="p-1 my-3 target:bg-dim-white rounded-1"
                 >
                     <p>
                         "Nullam vestibulum eleifend ligula, sed finibus enim."
@@ -49,7 +49,7 @@ pub fn Content() -> impl IntoView {
             >
                 <aside
                     id="saggitis"
-                    class="p-1 my-3 target:bg-base-b rounded-1"
+                    class="p-1 my-3 target:bg-dim-white rounded-1"
                 >
                     <p>
                         "Sagittis ac urna ut pellentesque. Donec quis pharetra mauris. Cras rhoncus vestibulum nibh luctus luctus. Nullam gravida ex vitae turpis consequat facilisis. Cras volutpat felis vitae mauris egestas, in tempus ligula tristique. Duis lobortis risus in nisl interdum pretium."
@@ -76,23 +76,23 @@ fn Anchor(to: &'static str) -> impl IntoView {
 #[component]
 pub fn OldContent() -> impl IntoView {
     mview! {
-        body class="h-dvh w-dvw flex flex-row overflow-x-scroll snap-x snap-mandatory bg-base-a text-base-e font-serif" (
+        body class="h-dvh w-dvw flex flex-row overflow-x-scroll snap-x snap-mandatory bg-white text-black font-serif" (
             div #summary class="h-full py-[50vh] w-1/2 shrink-0 snap-center ml-[50%] overflow-y-scroll overscroll-y-contain" (
                 p ("Lorem ipsum dolor sit amet, consectetur " Anchor to="pharetra"; " elit.")
                 p ("Fusce a libero " Anchor to="nullam"; ".")
             )
             div #exposition class="h-full py-[50vh] w-1/2 shrink-0 snap-center overflow-y-scroll overscroll-y-contain" (
-                section id="pharetra" class="my-3 target:bg-base-b rounded-1 p-1" (
+                section id="pharetra" class="my-3 target:bg-dim-white rounded-1 p-1" (
                     p ("Pharetra ex gravida congue. Pellentesque ornare lacus quis quam " Anchor to="saggitis"; " id aliquam urna.")
                     p ("Nulla ligula dui, consequat vel dolor vitae, luctus porta lacus. Suspendisse viverra dapibus neque. Curabitur faucibus facilisis tincidunt.")
                 )
-                section id="nullam" class="my-3 target:bg-base-b rounded-1 p-1" (
+                section id="nullam" class="my-3 target:bg-dim-white rounded-1 p-1" (
                     p ("Nullam vestibulum eleifend ligula, sed finibus enim.")
                     p ("Maecenas fermentum, odio in consequat ultricies, nunc neque luctus arcu, at egestas eros ex sed libero.")
                 )
             )
             div #details class="h-full py-[50vh] w-1/2 shrink-0 snap-center mr-[50%] overflow-y-scroll overscroll-y-contain" (
-                aside id="saggitis" class="my-3 target:bg-base-b rounded-1 p-1" (
+                aside id="saggitis" class="my-3 target:bg-dim-white rounded-1 p-1" (
                     p ("Sagittis ac urna ut pellentesque. Donec quis pharetra mauris. Cras rhoncus vestibulum nibh luctus luctus. Nullam gravida ex vitae turpis consequat facilisis. Cras volutpat felis vitae mauris egestas, in tempus ligula tristique. Duis lobortis risus in nisl interdum pretium.")
                     p ("Etiam sed elit at ligula interdum venenatis in a nisl. Vestibulum congue nunc ac mi rhoncus tincidunt. Nunc vel scelerisque velit. Cras sit amet sapien ultricies, commodo arcu sit amet, aliquam ex.")
                     p ("Sed dolor libero, pretium ut metus eu, tincidunt pulvinar magna. Aliquam a tortor ex. Praesent quis nisi id ante pellentesque aliquam. Fusce id porta nulla, ut facilisis neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae")
--- src/ux.rs
+++ src/ux.rs
@@ -1,6 +1,52 @@
 use leptos::prelude::*;
+use leptos_use::{UseClipboardOptions, UseClipboardReturn, use_clipboard_with_options};
 
 #[component]
-pub fn Code(_children: Children) -> impl IntoView {
-    view! {}
+pub fn Code(children: Children) -> impl IntoView {
+    view! {
+        <div class="relative p-1 bg-black to-black shadow-lg border-[0.5px] border-black/25 bg-radial-[at_10%_0%] to-20% from-dim-white/15 shadow-dim-grey rounded-1 inset-shadow-sm inset-shadow-white/20">
+            {children()}
+        </div>
+    }
+}
+
+#[component]
+pub fn Raw(code: String) -> impl IntoView {
+    let UseClipboardReturn { copied, copy, .. } =
+        use_clipboard_with_options(UseClipboardOptions::default().copied_reset_delay(3000.0));
+
+    view! {
+        <Show
+            when=copied
+            fallback=move || {
+                view! {
+                    <button
+                        class="absolute top-1 right-1 px-1 font-mono text-sm border rounded-1 border-grey"
+                        on:click={
+                            let copy = copy.clone();
+                            let code = code.clone();
+                            move |_| copy(&code)
+                        }
+                    >
+                        <p class="py-1 text-grey hover:text-dim-white">"copy"</p>
+                    </button>
+                }
+            }
+        >
+
+            <button class="absolute top-1 right-1 px-1 font-mono text-sm border rounded-1 border-dim-green">
+                <p class="py-1 text-dim-green">"done"</p>
+            </button>
+        </Show>
+    }
+}
+
+#[component]
+pub fn Line(#[prop(default = 0)] indent: usize, children: Children) -> impl IntoView {
+    view! {
+        <p class="font-mono" data-indent=indent>
+            <span class="whitespace-pre">{" ".repeat(indent)}</span>
+            {children()}
+        </p>
+    }
 }

eb0ca96e2d929b241f698e4b84b0560f5cde0749
scottfowler <scott@fwlr.io>
2026-02-19 04:39:49 +1100

    release tiem

--- src/codeblocks/test.rs
+++
@@ -1,4 +0,0 @@
-#[component]
-pub fn Code(children: Children) -> impl IntoView {
-    view! { <div>"hello, world"</div> }
-}
--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,26 @@ use fwlr_io::app::App;
 use leptos::mount::mount_to_body;
 
 fn main() {
-    tracing_subscriber::fmt()
-        .event_format(
-            tracing_subscriber::fmt::format()
-                .with_ansi(false)
-                .with_level(false)
-                .compact(),
-        )
-        .with_writer(
-            tracing_subscriber_wasm::MakeConsoleWriter::default()
-                .map_trace_level_to(tracing::Level::DEBUG),
-        )
-        .without_time()
-        .init();
+    #[cfg(debug_assertions)]
+    {
+        tracing_subscriber::fmt()
+            .event_format(
+                tracing_subscriber::fmt::format()
+                    .with_ansi(false)
+                    .with_level(false)
+                    .compact(),
+            )
+            .with_writer(
+                tracing_subscriber_wasm::MakeConsoleWriter::default()
+                    .map_trace_level_to(tracing::Level::DEBUG),
+            )
+            .without_time()
+            .init();
 
-    console_error_panic_hook::set_once();
+        console_error_panic_hook::set_once();
 
-    tracing::info!("mounting");
+        tracing::info!("mounting");
+    };
 
     mount_to_body(App)
 }
